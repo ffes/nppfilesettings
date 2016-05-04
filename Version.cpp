6b0b0e0be1708a9e7448bc5a3c29fca0c4680352
Frank Fesevur <ffes@users.sourceforge.net>
2016-05-04 12:46:19 +0200

    Remove unneeded include files

--- Version.cpp
+++ Version.cpp
@@ -22,8 +22,6 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
-#include "NPP/PluginInterface.h"
-#include "NppFileSettings.h"
 #include "Version.h"
 
 /////////////////////////////////////////////////////////////////////////////
